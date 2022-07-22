330ee40fb7cd1f495ff3487e5b29a7081bf4fa42
Fanafaire <85456148+Fanafaire@users.noreply.github.com>
2022-07-22 19:58:19 +0300

    added: Users

--- app/src/main/java/com/fanafaire/shoptask/adapter/NodeAdapter.java
+++ app/src/main/java/com/fanafaire/shoptask/adapter/NodeAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,7 +12,6 @@ import com.fanafaire.shoptask.R;
 import com.fanafaire.shoptask.nodes.ProductNode;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class NodeAdapter extends RecyclerView.Adapter<NodeAdapter.ViewHolder>{
 
--- app/src/main/java/com/fanafaire/shoptask/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/fanafaire/shoptask/ui/gallery/GalleryFragment.java
@@ -4,34 +4,54 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.fanafaire.shoptask.databinding.FragmentGalleryBinding;
+import com.fanafaire.shoptask.R;
+import com.fanafaire.shoptask.adapter.NodeAdapter;
+//import com.fanafaire.shoptask.databinding.FragmentGalleryBinding;
+import com.fanafaire.shoptask.nodes.ProductNode;
+
+import java.util.ArrayList;
 
 public class GalleryFragment extends Fragment {
 
-    private FragmentGalleryBinding binding;
+//    private FragmentGalleryBinding binding;
+
+    public RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
 
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        recyclerView = (RecyclerView) root.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        NodeAdapter adapter = new NodeAdapter(getContext(), setInitialData());
+        recyclerView.setAdapter(adapter);
+
         return root;
+    }
+
+    private ArrayList<ProductNode> setInitialData(){
+        ArrayList<ProductNode> productList = new ArrayList<>();
+        productList.add(new ProductNode (1, "First Name 1" + "Second Name 1", 1000));
+        productList.add(new ProductNode (2, "First Name 2" + "Second Name 2", 10));
+        productList.add(new ProductNode (3, "First Name 3" + "Second Name 3", 50));
+        productList.add(new ProductNode (4, "First Name 4" + "Second Name 4", 500));
+        productList.add(new ProductNode (5, "First Name 5" + "Second Name 5", 100));
+
+        return productList;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+//        binding = null;
     }
 }
