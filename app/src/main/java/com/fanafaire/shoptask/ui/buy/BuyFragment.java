7875b7513908717c6e2a3b3ee0ffdb31fb075866
Fanafaire <85456148+Fanafaire@users.noreply.github.com>
2022-07-23 18:21:48 +0300

    added: button

--- app/src/main/java/com/fanafaire/shoptask/ui/buy/BuyFragment.java
+++ app/src/main/java/com/fanafaire/shoptask/ui/buy/BuyFragment.java
@@ -4,41 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.fanafaire.shoptask.R;
-import com.fanafaire.shoptask.databinding.FragmentBuyBinding;
 
 public class BuyFragment extends Fragment {
-
-    private FragmentBuyBinding binding;
-//    private Button buyButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         BuyViewModel buyViewModel =
                 new ViewModelProvider(this).get(BuyViewModel.class);
 
-        binding = FragmentBuyBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View root = inflater.inflate(R.layout.fragment_buy, container, false);
 
-//        final TextView textView = binding.textBuy;
-//        buyViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Button buyButton = (Button)root.findViewById(R.id.buy_button);
 
-//        View rootView = inflater.inflate(R.layout.fragment_buy, container, false);
-//        Button buyButton = (Button)rootView.findViewById(R.id.buy_button);
-//
-//        buyButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(getContext(), "String", Toast.LENGTH_LONG).show();
-//
-//            }
-//        });
+        buyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "String", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
         return root;
     }
@@ -46,6 +38,5 @@ public class BuyFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
 }
